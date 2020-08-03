8cc6a48a6fe7784e4a126e63ff8a754772ebef80
Tom <tomas@indicius.com>
2020-08-03 08:54:39 -0300

    General / Adds  and connects to CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Indicius' Gatsby Starter`,
@@ -5,6 +9,13 @@ module.exports = {
     author: `@tomasperalta`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: '6cma8zf36gz0',
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN
+      }
+    },
     {
       resolve: `gatsby-plugin-react-svg`,
       options: {
@@ -33,7 +44,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
