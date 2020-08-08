427b1ac84d5f66131a4cc061ddb4f73c8613e6e8
Tom <tomas@indicius.com>
2020-08-08 19:29:58 -0300

    General / Enables persistent-layout by default

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,18 @@ module.exports = {
   siteMetadata: {
     title: `Indicius' Gatsby Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@tomasperalta`,
+    author: `@tomasperalta`
   },
   plugins: [
+    // persistent-layout is enabled by default
+    // to disable comment the following plugin,
+    // and manually wrap your pages with your layout(s)
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/layouts/layout-primary.js`)
+      }
+    },
     {
       resolve: `gatsby-plugin-react-svg`,
       options: {
@@ -18,8 +27,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
+        path: `${__dirname}/src/assets/images`
+      }
     },
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
@@ -33,11 +42,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        icon: `src/assets/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 }
