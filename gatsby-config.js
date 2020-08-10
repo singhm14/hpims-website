619b4a94055cb8141fa747983176aa1ac3424bc5
Tom <tomas@indicius.com>
2020-08-10 08:56:19 -0300

    Components / Adds SEO component

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,9 @@ module.exports = {
   siteMetadata: {
     title: `Indicius' Gatsby Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    url: 'https://indicius.com',
+    image: '',
+    twitterUsername: '@indiciusar',
     author: `@tomasperalta`
   },
   plugins: [
--- src/layouts/layout-primary.js
+++ src/layouts/layout-primary.js
@@ -4,8 +4,12 @@ import React from 'react'
 import 'assets/styles/reset.css'
 import GlobalStyles from 'assets/styles/globalStyles'
 
+// Components
+import SEO from 'components/seo/'
+
 const Layout = ({ children }) => (
   <main>
+    <SEO />
     <GlobalStyles />
     {children}
   </main>
