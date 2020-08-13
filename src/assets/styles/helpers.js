ab25f46d54fa6203e65177c407cb594d93501479
Tom <tomas@indicius.com>
2020-08-13 11:44:47 -0300

    General / Styles / Adds helpers

---
+++ src/assets/styles/helpers.js
@@ -0,0 +1,27 @@
+// Libraries
+import { css } from 'styled-components'
+
+// Utils
+import { colors } from 'utils/variables/'
+
+export const Helpers = () => {
+  let styles = ''
+
+  Object.entries(colors).forEach(([name, color]) => {
+    styles += `
+      // Text color classes
+      .color--${name} {
+        color: ${color};
+      }
+
+      // Background color classes
+      .bg--${name} {
+        background-color: ${color};
+      }
+    `
+  })
+
+  return css`
+    ${styles}
+  `
+}
--- src/utils/variables/index.js
+++ src/utils/variables/index.js
@@ -1,11 +1,17 @@
 // Breakpoints:start
 export const breakpoints = {
-  small: "768",
-  medium: "1024",
-  large: "1200",
-  extraLarge: "1440",
-  wide: "2000"
+  small: '768',
+  medium: '1024',
+  large: '1200',
+  extraLarge: '1440',
+  wide: '2000'
 }
 // Breakpoints:end
 
-// Add your variables here:
+// Colors:start
+export const colors = {
+  // Insert your variables here
+  white: '#FFFFFF',
+  black: '#000'
+}
+// Colors:end
