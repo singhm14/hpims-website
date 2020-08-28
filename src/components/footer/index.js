d984cf21ee4142d3bb40fd065f797a22df7023b5
Tom <tomas@indicius.com>
2020-08-28 11:55:43 -0300

    Components / Footer / Adds new layout

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -29,24 +29,56 @@ const StyledFooter = styled.footer`
     `}
   }
 
+  .email {
+    margin-bottom: 24px;
+
+    ${breakpoint.medium`
+      margin-bottom: 64px;
+    `}
+
+    p {
+      margin-bottom: 8px;
+      font-weight: 600;
+      text-transform: uppercase;
+    }
+
+    h4 {
+      font-weight: 400;
+    }
+  }
+
   ul {
+    display: flex;
     margin-bottom: 40px;
 
     ${breakpoint.medium`
-      display: flex;
       margin-bottom: 0;
     `}
 
+    .column {
+      margin-right: 32px;
+
+      &:last-child {
+        margin-right: 0;
+      }
+
+      ${breakpoint.small`
+        margin-right: 64px;
+      `}
+
+      ${breakpoint.medium`
+        margin-right: 152px;
+      `}
+    }
+
     li {
-      margin-bottom: 8px;
+      margin-bottom: 12px;
+      font-size: 14px;
+      font-weight: 600;
 
       &:last-child {
         margin-bottom: 0;
       }
-
-      ${breakpoint.medium`
-        margin: 0 8px 0 0;
-      `}
 
       ${breakpoint.large`
         margin-right: 32px;
@@ -76,38 +108,52 @@ const StyledFooter = styled.footer`
 const Footer = () => (
   <StyledFooter className="bg--grey100">
     <Container>
-      <ul>
-        <li>
-          <Link to="/" className="color--black color-hover--magenta900" activeClassName="active">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className="color--black color-hover--magenta900" activeClassName="active">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/team" className="color--black color-hover--magenta900" activeClassName="active">
-            Team
-          </Link>
-        </li>
-        <li>
-          <Link to="/research" className="color--black color-hover--magenta900" activeClassName="active">
-            Research
-          </Link>
-        </li>
-        <li>
-          <Link to="/publications" className="color--black color-hover--magenta900" activeClassName="active">
-            Publications
-          </Link>
-        </li>
-        <li>
-          <Link to="/careers" className="color--black color-hover--magenta900" activeClassName="active">
-            Careers
-          </Link>
-        </li>
-      </ul>
+      <div className="footer__sitemap">
+        <div className="email">
+          <p className="paragraph--small color--black">Contact Us</p>
+          <h4>
+            <a href="mailto:hpimsinfo@mssm.edu" className="color--blue300 color-hover--magenta300">
+              HPIMSinfo@mssm.edu
+            </a>
+          </h4>
+        </div>
+        <ul>
+          <div className="column">
+            <li>
+              <Link to="/" className="color--blue500 color-hover--magenta300" activeClassName="active">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="color--blue500 color-hover--magenta300" activeClassName="active">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/team" className="color--blue500 color-hover--magenta300" activeClassName="active">
+                Team
+              </Link>
+            </li>
+          </div>
+          <div className="column">
+            <li>
+              <Link to="/research" className="color--blue500 color-hover--magenta300" activeClassName="active">
+                Research
+              </Link>
+            </li>
+            <li>
+              <Link to="/publications" className="color--blue500 color-hover--magenta300" activeClassName="active">
+                Publications
+              </Link>
+            </li>
+            <li>
+              <Link to="/careers" className="color--blue500 color-hover--magenta300" activeClassName="active">
+                Careers
+              </Link>
+            </li>
+          </div>
+        </ul>
+      </div>
 
       <div className="footer__logos">
         <div className="logos">
