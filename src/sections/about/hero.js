34c045f3492538de580854c5eb752fa98e7163ab
Tom <tomas@indicius.com>
2020-09-17 11:46:26 -0300

    About / Hero / Fixes font-size issue

--- src/sections/about/hero.js
+++ src/sections/about/hero.js
@@ -57,6 +57,14 @@ const StyledHero = styled(Hero)`
   .title {
     max-width: 640px;
   }
+
+  p {
+    font-size: 14px;
+
+    ${breakpoint.medium`
+      font-size: 16px;
+    `}
+  }
 `
 
 export default () => (
@@ -69,7 +77,8 @@ export default () => (
       <p className="subtitle">About</p>
       <h1 className="title color--blue500">Turning the promise of digital health into a reality</h1>
       <p>
-        This extraordinary collaboration allows us to combine data science, biomedical and digital engineering, and health care expertise to offer unprecedented opportunities for healthcare. <br />
+        This extraordinary collaboration allows us to combine data science, biomedical and digital engineering, and health care expertise to offer unprecedented opportunities for healthcare.
+        <br />
         Our goal is to turn the promise of digital health into a reality, being the driver of innovations that will revolutionize the way in which people think about their personal health and health systems, generating real impact in people’s lives.
       </p>
     </Container>
