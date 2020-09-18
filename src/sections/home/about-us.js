ada35f0aa53aec2f49907163fd488e7b35e595d4
Tom <tomas@indicius.com>
2020-09-18 11:28:27 -0300

    Home / About Us / Removes link

--- src/sections/home/about-us.js
+++ src/sections/home/about-us.js
@@ -10,7 +10,6 @@ import breakpoint from 'utils/breakpoints/'
 // Components
 import Container from 'components/container/'
 import Img from 'gatsby-image'
-import { Tertiary } from 'components/buttons/'
 
 const StyledAboutUs = styled.section`
   padding-top: 64px;
@@ -41,10 +40,6 @@ const StyledAboutUs = styled.section`
       `}
     }
   }
-
-  .about__link {
-    margin-top: 16px;
-  }
 `
 
 const AboutUs = (props) => {
@@ -68,8 +63,6 @@ const AboutUs = (props) => {
           <p>Science has helped humanity to improve and extend life. Today, technological advances have allowed us to understand and analyze information in ways never before possible.</p>
           <br />
           <p>The Hasso Plattner Institute for Digital Health at Mount Sinai (HPI･MS) propels these possibilities through an extraordinary international academic collaboration between the Hasso Plattner Institute for Digital Engineering in Potsdam, Germany, and the Mount Sinai Health System in New York City, USA.</p>
-
-          <Tertiary to="/about" className="about__link color--blue300 color-hover--blue500 font-weight--600 svg--stroke-blue300 svg-hover--stroke-blue500" text="Learn more about us" />
         </div>
       </Container>
 
