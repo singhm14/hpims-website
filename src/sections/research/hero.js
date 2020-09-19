7ae209e34598ec90a3c3b553398ae9931ec55e5c
Tom <tomas@indicius.com>
2020-09-19 17:55:53 -0300

    Research / Hero / Fixes container size issue

--- src/sections/research/hero.js
+++ src/sections/research/hero.js
@@ -6,7 +6,7 @@ import Container from 'components/container/'
 
 const Hero = () => (
   <PageHero className="bg--blue500 color--white">
-    <Container>
+    <Container className="container">
       <p className="subtitle">Research</p>
       <h1 className="title">Leading the digital health revolution</h1>
       <p>Our research leverages the latest technologies to create novel methodologies and bring together interdisciplinary expertise.</p>
