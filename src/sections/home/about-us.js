10e55a84c137442c3265a03fe02adf9ec711b82d
Tom <tomas@indicius.com>
2020-09-24 12:23:54 -0300

    Home / About Us / Adds observer to Lottie

--- src/sections/home/about-us.js
+++ src/sections/home/about-us.js
@@ -75,35 +75,70 @@ const StyledAboutUs = styled.section`
   }
 `
 
-const AboutUs = (props) => {
-  const animationOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: AboutAnimation,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
+class AboutUs extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isVisible: false
+    }
+    this.contentWrapper = React.createRef()
+  }
+
+  componentDidMount = () => {
+    const observer = new IntersectionObserver(
+      ([entry], self) => {
+        if (entry.isIntersecting && !this.state.isVisible) {
+          this.handleVisibility()
+          self.unobserve(this.contentWrapper.current)
+        }
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+      }
+    )
+
+    observer.observe(this.contentWrapper.current)
+  }
+
+  handleVisibility = () => {
+    if (!this.state.isVisible) {
+      this.setState({
+        isVisible: true
+      })
     }
   }
 
-  return (
-    <StyledAboutUs>
-      <Container>
-        <div className="section__title">
-          <p className="section__subtitle color--black">About Us</p>
-          <h3 className="color--blue500">Turning the promise of digital health into a reality</h3>
-          <p>Science has helped humanity to improve and extend life. Today, technological advances have allowed us to understand and analyze information in ways never before possible.</p>
-          <br />
-          <p>The Hasso Plattner Institute for Digital Health at Mount Sinai (HPI･MS) propels these possibilities through an extraordinary international academic collaboration between the Hasso Plattner Institute for Digital Engineering in Potsdam, Germany, and the Mount Sinai Health System in New York City, USA.</p>
+  render = () => {
+    const animationOptions = {
+      loop: true,
+      autoplay: this.state.isVisible,
+      animationData: AboutAnimation,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    }
 
-          <Tertiary to="/about" className="about__link color--blue300 color-hover--blue500 font-weight--600 svg--stroke-blue300 svg-hover--stroke-blue500" text="Learn more about us" />
+    return (
+      <StyledAboutUs>
+        <Container>
+          <div className="section__title" ref={this.contentWrapper}>
+            <p className="section__subtitle color--black">About Us</p>
+            <h3 className="color--blue500">Turning the promise of digital health into a reality</h3>
+            <p>Science has helped humanity to improve and extend life. Today, technological advances have allowed us to understand and analyze information in ways never before possible.</p>
+            <br />
+            <p>The Hasso Plattner Institute for Digital Health at Mount Sinai (HPI･MS) propels these possibilities through an extraordinary international academic collaboration between the Hasso Plattner Institute for Digital Engineering in Potsdam, Germany, and the Mount Sinai Health System in New York City, USA.</p>
+
+            <Tertiary to="/about" className="about__link color--blue300 color-hover--blue500 font-weight--600 svg--stroke-blue300 svg-hover--stroke-blue500" text="Learn more about us" />
+          </div>
+        </Container>
+
+        <div className="about-us__image">
+          <Lottie options={animationOptions} isClickToPauseDisabled={true} />
         </div>
-      </Container>
-
-      <div className="about-us__image">
-        <Lottie options={animationOptions} isClickToPauseDisabled={true} />
-      </div>
-    </StyledAboutUs>
-  )
+      </StyledAboutUs>
+    )
+  }
 }
-
 export default AboutUs
