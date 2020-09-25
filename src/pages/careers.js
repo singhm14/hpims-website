02cb92cd0adb94e7fb7da00a3258c8fc4efd65f9
Tom <tomas@indicius.com>
2020-09-25 07:17:08 -0300

    Careers / Adds 'Careers List' section

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 // Sections
 import Hero from 'sections/careers/hero'
+import CareersList from 'sections/careers/careers-list'
 
 const Careers = () => (
   <React.Fragment>
     <Hero />
+    <CareersList />
   </React.Fragment>
 )
 
---
+++ src/sections/careers/careers-list.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+// Libraries
+import { useStaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
+
+// Utils
+import breakpoint from 'utils/breakpoints'
+
+// Components
+import Container from 'components/container/'
+import Grid from 'components/grid/'
+import CareersCard from 'components/careers-card/'
+
+const StyledCareersList = styled.section`
+  padding: 60px 0;
+
+  ${breakpoint.medium`
+    padding: 80px 0;
+  `}
+`
+
+const CareersList = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulCareers {
+        nodes {
+          id
+          jobTitle
+          jobSummary {
+            jobSummary
+          }
+          requirementsSummary {
+            json
+          }
+          applicationInstructions {
+            json
+          }
+          jobDetails {
+            json
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <StyledCareersList>
+      <Container>
+        <Grid gutter="32" columns="1">
+          {data.allContentfulCareers.nodes.map((career) => (
+            <div className="grid__item">
+              <CareersCard key={career.id} title={career.jobTitle} summary={career.jobSummary.jobSummary} requirements={career.requirementsSummary} applicationInstructions={career.applicationInstructions} details={career.jobDetails} />
+            </div>
+          ))}
+        </Grid>
+      </Container>
+    </StyledCareersList>
+  )
+}
+
+export default CareersList
