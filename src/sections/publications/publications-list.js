c844059349858a81f8d3024e9615e41f24ebe6f2
Tom Peralta <tomas@indicius.com>
2020-09-27 14:00:22 -0300

    [WIP] Publications / Publications List / Adds post-filtering

--- src/sections/publications/publications-list.js
+++ src/sections/publications/publications-list.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from 'react'
 // Libraries
 import { useStaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
+import queryString from 'query-string'
+
+// Utils
+import { getSlug } from 'utils/functions'
 
 // Components
 import Grid from 'components/grid/'
@@ -13,7 +17,12 @@ const StyledPublicationsList = styled.section`
 `
 
 const PublicationsList = () => {
+  // We'll save the filters as states
   const [postsShowing, setPostsShowing] = useState(6)
+  const [yearParameter, setYearParameter] = useState(null)
+  const [categoryParamater, setCategoryParameter] = useState(null)
+  const [authorParameter, setAuthorParameter] = useState(null)
+  const [publicationMethodParameter, setPublicationMethodParameter] = useState(null)
   const infiniteTrigger = React.createRef()
 
   const data = useStaticQuery(graphql`
@@ -49,6 +58,7 @@ const PublicationsList = () => {
     }
   `)
 
+  // We'll log the observer and the loadMorePosts() function
   useEffect(() => {
     console.log('Showing ' + postsShowing + ' posts...')
     const loadMorePosts = () => {
@@ -66,6 +76,19 @@ const PublicationsList = () => {
 
     observer.observe(infiniteTrigger.current)
   })
+
+  // We'll save queryStrings as state
+  useEffect(() => {
+    setYearParameter(queryString.parse(window.location.search).year)
+    setCategoryParameter(queryString.parse(window.location.search).category)
+    setAuthorParameter(queryString.parse(window.location.search).author)
+    setPublicationMethodParameter(queryString.parse(window.location.search).publicationMethod)
+
+    // We'll filter the data array
+    // searching for posts matching the queryString
+    if (yearParameter || categoryParamater || authorParameter || publicationMethodParameter) {
+    }
+  }, [yearParameter, categoryParamater, authorParameter, publicationMethodParameter])
 
   return (
     <StyledPublicationsList>
