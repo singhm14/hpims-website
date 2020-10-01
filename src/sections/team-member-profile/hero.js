9c51640c9ddb03c02f0832113ce4937f5fd39e3c
Tom <tomas@indicius.com>
2020-10-01 19:38:45 -0300

    Team Member Profile / Adds 'Hero' section

---
+++ src/sections/team-member-profile/hero.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// Components
+import PageHero from 'components/hero/'
+
+const Hero = () => <PageHero className="gradient--primary color--white"></PageHero>
+
+export default Hero
---
+++ src/templates/team-member-profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Sections
+import Hero from 'sections/team-member-profile/hero'
+
+const TeamMemberProfile = () => (
+  <React.Fragment>
+    <Hero />
+  </React.Fragment>
+)
+
+export default TeamMemberProfile
