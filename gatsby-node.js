fc0a9dc69206256f78c7eb8574e20b1692efc028
Tom <tomas@indicius.com>
2020-10-01 19:38:19 -0300

    General / Creates pages for Team Members

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,10 +7,58 @@
 // You can delete this file if you're not using it
 const path = require('path')
 
+// Absolute path
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules']
     }
+  })
+}
+
+// Team Member Profile
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const template = path.resolve('./src/templates/team-member-profile.js')
+  const response = await graphql(`
+    query {
+      allContentfulTeamMembers {
+        nodes {
+          id
+          name
+        }
+      }
+    }
+  `)
+
+  const getSlug = (tag) => {
+    if (tag) {
+      tag = tag.replace(/^\s+|\s+$/g, '')
+      tag = tag.toLowerCase()
+
+      // remove accents, swap ñ for n, etc
+      var from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;'
+      var to = 'aaaaeeeeiiiioooouuuunc------'
+      for (var i = 0, l = from.length; i < l; i++) {
+        tag = tag.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
+      }
+
+      tag = tag
+        .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-') // collapse dashes
+    }
+
+    return tag
+  }
+
+  response.data.allContentfulTeamMembers.nodes.forEach((node) => {
+    createPage({
+      component: template,
+      path: `/team/${getSlug(node.name)}`,
+      context: {
+        id: node.id
+      }
+    })
   })
 }
