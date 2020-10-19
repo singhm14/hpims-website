6b8003836028c84878ff4a4022759598c9f408f0
Tom <tomas@indicius.com>
2020-10-19 08:59:30 -0300

    Team / Hero / Fixes typo

--- src/sections/team/hero.js
+++ src/sections/team/hero.js
@@ -8,7 +8,7 @@ const Hero = () => (
   <PageHero className="gradient--primary color--white">
     <Container className="container">
       <p className="subtitle">TEAM</p>
-      <h2 className="title">Leaders in<br/>digital health</h2>
+      <h2 className="title">Leaders in<br/>Digital Health</h2>
       <p className="paragraph--large">HPI·MS is home to a diverse team of faculty, research staff, postdocs, and students combining expertise in computer science, machine learning, digital engineering, clinical informatics, and medicine. </p>
     </Container>
   </PageHero>
