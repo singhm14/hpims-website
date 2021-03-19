553743d8b2345aeb01e177316b2bb664e3f1c671
Tom <tomas@indicius.com>
2021-03-19 08:32:23 -0300

    Home / Adds 'Banner' section

--- src/components/button-link/index.js
+++ src/components/button-link/index.js
@@ -9,6 +9,8 @@ const ButtonLink = styled(Link)`
   padding: 8px 24px;
   font-weight: 600;
   text-align: center;
+  box-sizing: border-box;
+  transition: all 0.6s ease;
 `
 
 export default ButtonLink
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 // Libraries
 import Helmet from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
 
 // Sections
+import Banner from 'sections/home/banner'
 import Hero from 'sections/home/hero'
 import AboutUs from 'sections/home/about-us'
 import Faculty from 'sections/home/faculty'
@@ -12,20 +14,39 @@ import ResearchProjects from 'sections/home/research-projects'
 import Publications from 'sections/home/publications'
 import News from 'sections/home/news'
 
-const Home = () => (
-  <React.Fragment>
-    <Helmet>
-      <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-    </Helmet>
-    <Hero />
-    <AboutUs />
-    <Faculty />
-    <WhyHPIMS />
-    <ResearchProjects />
-    <Publications />
-    <News />
-  </React.Fragment>
-)
+const Home = () => {
+  const { allContentfulHomesFacultyBanner: banner } = useStaticQuery(graphql`
+    query {
+      allContentfulHomesFacultyBanner {
+        nodes {
+          title
+          content {
+            content
+          }
+          link
+        }
+      }
+    }
+  `)
+
+  console.log(banner)
+
+  return (
+    <React.Fragment>
+      <Helmet>
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+      </Helmet>
+      {banner && <Banner data={banner.nodes[0]} />}
+      <Hero />
+      <AboutUs />
+      <Faculty />
+      <WhyHPIMS />
+      <ResearchProjects />
+      <Publications />
+      <News />
+    </React.Fragment>
+  )
+}
 
 export default Home
