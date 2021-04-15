4cc7278d8a2b2cd92bc65b8e5862540772779d1e
Tom <tomas@indicius.com>
2021-04-15 09:13:09 -0300

    Faculty / Adds OG tags

--- src/pages/{contentfulFacultyPage.slug}.js
+++ src/pages/{contentfulFacultyPage.slug}.js
@@ -4,6 +4,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 // Components
+import SEO from 'components/seo/'
 import FacultySection from 'components/faculty-section/'
 
 // Sections
@@ -13,6 +14,7 @@ const Faculty = (props) => {
   const { contentfulFacultyPage: data } = props.data
   return (
     <React.Fragment>
+      <SEO description="HPIMS is seeking to recruit exceptional faculty members to join the institute’s mission to impact human health. Learn more by visiting our website!" image="https://images.ctfassets.net/6cma8zf36gz0/278E5hjP55aXUOzMY0R9YF/4eed05efd940d8fe47b2995a3dc01080/Img_copy.png?w=928&h=494&q=100&fm=webp" />
       <Hero data={data} />
       {data.sections.map((section) => (
         <FacultySection data={section} />
