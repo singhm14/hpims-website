0022c49f43047f18ea9c1f95df525253b0f7a46b
Tom <tomas@indicius.com>
2023-03-04 11:16:47 -0300

    Research Projects / Fixes embedded asset issue

--- src/templates/research-project.js
+++ src/templates/research-project.js
@@ -1,77 +1,82 @@
-import React from "react";
+import React from "react"
 
 // Libraries
-import styled from "styled-components";
-import { graphql } from "gatsby";
+import styled from "styled-components"
+import { graphql } from "gatsby"
 
 // Sections
-import Hero from "sections/research-project/hero";
-import Content from "sections/research-project/content";
-import Sidebar from "sections/research-project/sidebar";
+import Hero from "sections/research-project/hero"
+import Content from "sections/research-project/content"
+import Sidebar from "sections/research-project/sidebar"
 
 // Components
-import Seo from "components/seo/";
+import Seo from "components/seo/"
 
 // Components
-import Container from "components/container/";
+import Container from "components/container/"
 
 const StyledContainer = styled(Container)`
-  max-width: 928px;
-`;
+	max-width: 928px;
+`
 
 export const query = graphql`
-  query ($id: String!) {
-    contentfulResearchProjects(id: { eq: $id }) {
-      title
-      summary {
-        summary
-      }
-      icon {
-        gatsbyImageData(width: 400, quality: 100)
-      }
-      description {
-        raw
-      }
-      teamMembers {
-        id
-        name
-        profilePicture {
-          gatsbyImageData(width: 128, quality: 100)
-        }
-      }
-      callToAction {
-        id
-        title
-        description {
-          description
-        }
-        linkLabel
-        linkUrl
-      }
-    }
-  }
-`;
+	query ($id: String!) {
+		contentfulResearchProjects(id: { eq: $id }) {
+			title
+			summary {
+				summary
+			}
+			icon {
+				gatsbyImageData(width: 400, quality: 100)
+			}
+			description {
+				raw
+				references {
+					__typename
+					contentful_id
+					gatsbyImageData(width: 624, quality: 100)
+				}
+			}
+			teamMembers {
+				id
+				name
+				profilePicture {
+					gatsbyImageData(width: 128, quality: 100)
+				}
+			}
+			callToAction {
+				id
+				title
+				description {
+					description
+				}
+				linkLabel
+				linkUrl
+			}
+		}
+	}
+`
 
 const ResearchProject = (props) => (
-  <div data-aos="fade">
-    <Seo
-      title={
-        props.data.contentfulResearchProjects.title +
-        " | Hasso Plattner Institute for Digital Health at Mount Sinai"
-      }
-    />
-    <Hero data={props.data} />
-    <StyledContainer>
-      <div className="sidebar-layout sidebar-layout--big">
-        <div className="sidebar">
-          <Sidebar data={props.data} />
-        </div>
-        <div className="content">
-          <Content data={props.data} />
-        </div>
-      </div>
-    </StyledContainer>
-  </div>
-);
+	<div data-aos="fade">
+		<Seo
+			title={
+				props.data.contentfulResearchProjects.title +
+				" | Hasso Plattner Institute for Digital Health at Mount Sinai"
+			}
+		/>
+		<Hero data={props.data} />
+		<StyledContainer>
+			<div className="sidebar-layout sidebar-layout--big">
+				<div className="sidebar">
+					<Sidebar data={props.data} />
+				</div>
+				<div className="content">
+					<Content data={props.data} />
+				</div>
+			</div>
+		</StyledContainer>
+	</div>
+)
 
-export default ResearchProject;
+export default ResearchProject
