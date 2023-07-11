824e48313bfb8748e96d87060359b7a4203d4579
Juan José Peralta <juanjose@indicius.com>
2023-07-11 15:58:37 -0300

    team / add filter to ignore dummy post

--- src/sections/team/filters.js
+++ src/sections/team/filters.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 // Libraries
-import { useStaticQuery, graphql } from 'gatsby'
-import styled from 'styled-components'
-import queryString from 'query-string'
+import { useStaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
+import queryString from "query-string";
 
 // Utils
-import breakpoint from 'utils/breakpoints/'
-import { getSlug } from 'utils/functions/'
+import breakpoint from "utils/breakpoints/";
+import { getSlug } from "utils/functions/";
 
 // Components
-import FiltersWrapper from 'components/filters-wrapper/'
-import Dropdown from 'components/dropdown/'
-import { PrimaryExternal } from 'components/buttons/'
+import FiltersWrapper from "components/filters-wrapper/";
+import Dropdown from "components/dropdown/";
+import { PrimaryExternal } from "components/buttons/";
 
 const StyledFilters = styled.div`
   width: 100%;
@@ -36,36 +36,55 @@ const StyledFilters = styled.div`
       display: none;
     }
   }
-`
+`;
 
 const Filters = () => {
   // Default States
-  const defaultRole = typeof window !== 'undefined' ? queryString.parse(window.location.search).role : null
-  const defaultLab = typeof window !== 'undefined' ? queryString.parse(window.location.search).lab : null
-  const defaultProject = typeof window !== 'undefined' ? queryString.parse(window.location.search).project : null
+  const defaultRole =
+    typeof window !== "undefined"
+      ? queryString.parse(window.location.search).role
+      : null;
+  const defaultLab =
+    typeof window !== "undefined"
+      ? queryString.parse(window.location.search).lab
+      : null;
+  const defaultProject =
+    typeof window !== "undefined"
+      ? queryString.parse(window.location.search).project
+      : null;
 
   // Store filters as state
-  const [role, handleRoleFilter] = useState(defaultRole)
-  const [lab, handleLabFilter] = useState(defaultLab)
-  const [project, handleProjectFilter] = useState(defaultProject)
+  const [role, handleRoleFilter] = useState(defaultRole);
+  const [lab, handleLabFilter] = useState(defaultLab);
+  const [project, handleProjectFilter] = useState(defaultProject);
 
   // We'll build the query string here
-  const [urlQueryString, handleQueryString] = useState('')
+  const [urlQueryString, handleQueryString] = useState("");
 
   // This function build the query string
   useEffect(() => {
-    let toReturn = '?'
+    let toReturn = "?";
 
-    role && (toReturn += 'role=' + role)
-    lab && (toReturn === '?' ? (toReturn += 'lab=' + lab) : (toReturn += '&lab=' + lab))
-    project && (toReturn === '?' ? (toReturn += 'project=' + project) : (toReturn += '&project=' + project))
+    role && (toReturn += "role=" + role);
+    lab &&
+      (toReturn === "?"
+        ? (toReturn += "lab=" + lab)
+        : (toReturn += "&lab=" + lab));
+    project &&
+      (toReturn === "?"
+        ? (toReturn += "project=" + project)
+        : (toReturn += "&project=" + project));
 
-    handleQueryString(toReturn)
-  }, [role, lab, project])
+    handleQueryString(toReturn);
+  }, [role, lab, project]);
 
   const data = useStaticQuery(graphql`
     query {
-      allContentfulTeamMembers {
+      allContentfulTeamMembers(
+        filter: {
+          labs: { elemMatch: { title: { nin: "Dummy Post, DO NOT DELETE" } } }
+        }
+      ) {
         nodes {
           department
           labs {
@@ -77,45 +96,84 @@ const Filters = () => {
         }
       }
     }
-  `)
+  `);
 
-  let roles = []
-  let labs = []
-  let projects = []
+  let roles = [];
+  let labs = [];
+  let projects = [];
 
   // Populates the arrays with information
   data.allContentfulTeamMembers.nodes.map((teamMember) => {
     // Push all roles
-    teamMember.department && teamMember.department.map((department) => roles.push(department))
+    teamMember.department &&
+      teamMember.department.map((department) => roles.push(department));
 
     // Push all labs
-    teamMember.labs && teamMember.labs.map((lab) => labs.push(lab.title))
+    teamMember.labs && teamMember.labs.map((lab) => labs.push(lab.title));
 
     // Push all projects
-    teamMember.research_projects && teamMember.research_projects.map((project) => projects.push(project.title))
+    teamMember.research_projects &&
+      teamMember.research_projects.map((project) =>
+        projects.push(project.title)
+      );
 
-    return false // To avoid error
-  })
+    return false; // To avoid error
+  });
 
-  roles = Array.from(new Set(roles))
-  labs = Array.from(new Set(labs))
-  projects = Array.from(new Set(projects))
+  roles = Array.from(new Set(roles));
+  labs = Array.from(new Set(labs));
+  projects = Array.from(new Set(projects));
 
   return (
     <FiltersWrapper filterText="Filter members">
       <StyledFilters>
-        <p className="filters__title paragraph--small font-weight--600">{role || lab || project ? 'Filtered by' : 'Filter by'}</p>
+        <p className="filters__title paragraph--small font-weight--600">
+          {role || lab || project ? "Filtered by" : "Filter by"}
+        </p>
 
-        <Dropdown label="Role" defaultOption={defaultRole} options={roles} callbackFunction={(event) => handleRoleFilter(getSlug(event.target.innerText))} resetFunction={() => handleRoleFilter(null)} />
+        <Dropdown
+          label="Role"
+          defaultOption={defaultRole}
+          options={roles}
+          callbackFunction={(event) =>
+            handleRoleFilter(getSlug(event.target.innerText))
+          }
+          resetFunction={() => handleRoleFilter(null)}
+        />
 
-        <Dropdown label="Lab" defaultOption={defaultLab} options={labs} callbackFunction={(event) => handleLabFilter(getSlug(event.target.innerText))} resetFunction={() => handleLabFilter(null)} />
+        <Dropdown
+          label="Lab"
+          defaultOption={defaultLab}
+          options={labs}
+          callbackFunction={(event) =>
+            handleLabFilter(getSlug(event.target.innerText))
+          }
+          resetFunction={() => handleLabFilter(null)}
+        />
 
-        <Dropdown label="Project" defaultOption={defaultProject} options={projects} callbackFunction={(event) => handleProjectFilter(getSlug(event.target.innerText))} resetFunction={() => handleProjectFilter(null)} />
+        <Dropdown
+          label="Project"
+          defaultOption={defaultProject}
+          options={projects}
+          callbackFunction={(event) =>
+            handleProjectFilter(getSlug(event.target.innerText))
+          }
+          resetFunction={() => handleProjectFilter(null)}
+        />
 
-        <PrimaryExternal disabled={role === defaultRole && lab === defaultLab && project === defaultProject} className="filters__button bg-hover--blue500 color--blue500 color-hover--white border--blue500 border-hover--blue500" href={urlQueryString === '?' ? '/team' : '/team/' + urlQueryString} text="Apply Filter" />
+        <PrimaryExternal
+          disabled={
+            role === defaultRole &&
+            lab === defaultLab &&
+            project === defaultProject
+          }
+          className="filters__button bg-hover--blue500 color--blue500 color-hover--white border--blue500 border-hover--blue500"
+          href={urlQueryString === "?" ? "/team" : "/team/" + urlQueryString}
+          text="Apply Filter"
+        />
       </StyledFilters>
     </FiltersWrapper>
-  )
-}
+  );
+};
 
-export default Filters
+export default Filters;
